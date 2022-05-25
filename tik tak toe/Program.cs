d7f47ed179c0d69232f7ba86c6dd06b4ecb7e14d
Michal <mdomaniski@gmil.com>
2022-05-25 18:43:02 +0200

    v0.00.060.0

--- tik tak toe/Program.cs
+++ tik tak toe/Program.cs
@@ -3,15 +3,39 @@ using System.Runtime.InteropServices;
 
 internal class Program
 {
+    static void BotPlay()
+    {
+        bool botis = false;
+        while (!botis)
+        {
+            botX = r.Next(0, 2);
+            botY = r.Next(0, 2);
+            if (field[botY, botX] == " ")
+            {
+                field[botY, botX] = "O";
+                botis = true;
+            }
+        }
+    }
+    static void WinCheck()
+    {
+        Console.WriteLine("checked");
+    }
+
     static void Print()
     {
         Console.Clear();
-        Console.Write("  {0} | {1} | {2} \n -----------\n  {3} | {4} | {5} \n -----------\n  {6} | {7} | {8} \n",field[0,0],field[0,1],field[0,2],field[1,0],field[1,1],field[1,2],field[2,0],field[2,1],field[2,2]);
+        Console.Write("   1   2   3  x\n1  {0} | {1} | {2} \n  ---|---|---\n2  {3} | {4} | {5} \n  ---|---|---\n3  {6} | {7} | {8} \ny\n\n",field[0,0],field[0,1],field[0,2],field[1,0],field[1,1],field[1,2],field[2,0],field[2,1],field[2,2]);
     }
     static string[,] field = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
+    static int x;
+    static int y;
+    static int botX;
+    static int botY;
+    static bool ended = false;
+    static Random r = new Random();
     static void Main(string[] args)
     {
-        bool ended = false;
         while (!ended)
         {
             Print();
@@ -19,9 +43,10 @@ internal class Program
             while (!xis)
             {
                 Console.Write("x=");
-                int x = Convert.ToInt16(Console.ReadLine());
+                x = Convert.ToInt16(Console.ReadLine());
                 if (x > 0 && x < 4)
                 {
+                    x--;
                     xis = true;
                 }
                 else
@@ -33,15 +58,33 @@ internal class Program
             while (!yis)
             {
                 Console.Write("y=");
-                int x = Convert.ToInt16(Console.ReadLine());
-                if (x > 0 && x < 4)
+                y = Convert.ToInt16(Console.ReadLine());
+                if (y > 0 && y < 4)
                 {
+                    y--;
                     yis = true;
                 }
                 else
                 {
                     Console.WriteLine("choose y between 1 and 3");
                 }
+            }
+            if(field[y,x] == " ")
+            {
+                field[y, x] = "X";
+                WinCheck();
+                if (!ended)
+                {
+                    Print();
+                    Thread.Sleep(1500);
+                    BotPlay();
+                    WinCheck();
+                }
+            }
+            else
+            {
+                Console.WriteLine("choose empty field");
+                Console.ReadKey();
             }
         }
     }
